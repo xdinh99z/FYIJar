ae4b766175e1e98a9bcbaa2e01bc6f0fd336d00d
Dinh <xdinh@dxc.com>
2019-06-12 11:12:34 +0700

    commit spring boot

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#ROOT_FOLDER=$PWD
+#SOURCE_FOLDER=$PWD/todoApp
+#OUTPUT_BUILD_FOLDER=$SOURCE_FOLDER/dist
+#DIST_FOLDER=$PWD/dist
+
+#mkdir -p $DIST_FOLDER
+cd JarApp
+
+mvn package
+
+#npm install
+#npm install @angular/cli
+#npm run build
+
+#cd dist
+
+ls -la
+#cp -R $OUTPUT_BUILD_FOLDER/* $DIST_FOLDER/
